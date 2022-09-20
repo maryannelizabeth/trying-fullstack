a9c2e7298f0a6d5b89ee84fd5cbe1af4dfdd0d2c
Maryann Henderson <maryannhenderson@Maryanns-MacBook-Air.local>
2022-09-21 09:02:36 +1200

    trying to fix timer

--- client/components/StartTimer.jsx
+++ client/components/StartTimer.jsx
@@ -1,24 +1,11 @@
 import React from 'react'
 
-function StartTimer({ seconds, setSeconds, minutes, setMinutes }) {
-  function updateCountdown() {
-    if (seconds == 0) {
-      seconds = 5
-      setMinutes(minutes--)
-    } else if (seconds != 0) {
-      setSeconds(seconds--)
-    }
-    // minutes = seconds * 60
-    // if (minutes != 0) {
-    //   setMinutes(minutes--)
-    // } else if (minutes == 0) {
-    //   setMinutes(0)
-    // }
-  }
+function StartTimer({ updateCountdown }) {
   let startClick = () => {
     updateCountdown()
     setInterval(updateCountdown, 1000)
   }
+
   return (
     <div className="startCountdownButton">
       <button className="button-1" type="button" onClick={startClick}>
--- client/components/UserTimerInput.jsx
+++ client/components/UserTimerInput.jsx
@@ -1,33 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TimerDisplay from './TimerDisplay'
 import StartTimer from './StartTimer'
 
 function UserTimerInput() {
-  let [minutes, setMinutes] = useState()
-  let [seconds, setSeconds] = useState()
+  let [minutes, setMinutes] = useState(0)
+  let [seconds, setSeconds] = useState(0)
 
-  if (seconds < 10) {
-    seconds = `0${seconds}`
+  useEffect(() => {
+    console.log('line 10', seconds)
+    if (minutes != 0 && seconds == 0) {
+      setMinutes(minutes - 1)
+      console.log('useeffect ', minutes)
+      setSeconds(5)
+    }
+  }, [seconds])
+  useEffect(() => {
+    console.log('useeffect mins', minutes)
+  }, [minutes])
+
+  async function updateCountdown() {
+    // console.log('seconds', seconds)
+    // if (minutes + seconds == 0) {
+    //   await setSeconds(0)
+    // }
+    // if (seconds != 0) {
+    await setSeconds(seconds - 1)
+    console.log('countdown', seconds)
+    // }
+    // else if (seconds === 0) {
+    //   //seconds = 3
+
+    //   console.log('line 15', seconds)
+    //   await setSeconds(5)
+    //   console.log('countdown mins', seconds)
+    // }
   }
-  if (minutes < 10) {
-    minutes = `0${minutes}`
-  }
+
+  // if (seconds < 10) {
+  //   seconds = `0${seconds}`
+  // }
+  // if (minutes < 10) {
+  //   minutes = `0${minutes}`
+  // }
 
   const [print, setPrint] = useState(false)
 
   function getMinuteInput(minuteInput) {
-    setMinutes(Number(minuteInput.target.value))
+    setMinutes(minuteInput.target.value)
 
     setPrint(false)
   }
   function getSecondInput(input) {
-    setSeconds(Number(input.target.value))
+    setSeconds(input.target.value)
     setPrint(false)
   }
 
   function clickHandler(evt) {
     evt.preventDefault()
-
+    setSeconds(Number(seconds))
+    setMinutes(Number(minutes))
     setPrint(true)
   }
 
@@ -46,6 +77,7 @@ function UserTimerInput() {
                   max="59"
                   required
                   onChange={getMinuteInput}
+                  value={minutes}
                 />
               </label>
 
@@ -58,6 +90,7 @@ function UserTimerInput() {
                   max="59"
                   required
                   onChange={getSecondInput}
+                  value={seconds}
                 />
               </label>
             </div>
@@ -77,16 +110,17 @@ function UserTimerInput() {
       </div>
       <div>
         <TimerDisplay
-          timerMinutes={print ? minutes : '00'} // if something is less than 10 display 0+print seconds
-          timerSeconds={print ? seconds : '00'}
+          timerMinutes={print ? minutes : 0} // if something is less than 10 display 0+print seconds
+          timerSeconds={print ? seconds : 0}
         />
       </div>
       <div>
         <StartTimer
-          seconds={seconds}
-          minutes={minutes}
-          setMinutes={setMinutes}
-          setSeconds={setSeconds}
+          // seconds={seconds}
+          // minutes={minutes}
+          // setMinutes={setMinutes}
+          // setSeconds={setSeconds}
+          updateCountdown={updateCountdown}
         />
       </div>
     </>
